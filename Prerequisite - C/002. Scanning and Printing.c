dd2e46135650a6ca20eb91b5878adc5ab8264e45
Ankit Kumar <88437909+ankit-daniel@users.noreply.github.com>
2021-08-26 19:55:56 +0530

    Create 002. Scanning and Printing.c
    Program takes a character and a int value and displays them to the console.

---
+++ Prerequisite - C/002. Scanning and Printing.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char character;
+    int num;
+    printf("Enter the Character: ");
+    scanf("%c", &character);
+	  printf("\n The Entered character is: %c.\n", character);
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Entered Number is: %d.\n", num);
+}
